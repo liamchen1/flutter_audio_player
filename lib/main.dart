b9f27a22c4d3f9219f8a160d832fa0769c3ab40b
liamchen1 <czw_bjtu@yeah.net>
2024-09-28 16:05:47 +0800

    在三大平台上，设置窗体大小

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:window_size/window_size.dart';
 
 import 'models/playlist_provider.dart';
 import 'pages/home_page.dart';
 import 'themes/theme_provider.dart';
 
 void main() {
+  setupWindow();
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => ThemeProvider()),
@@ -27,5 +36,24 @@ class MyApp extends StatelessWidget {
       theme: Provider.of<ThemeProvider>(context).themeData,
       home: const HomePage(),
     );
+  }
+}
+
+const double windowWidth = 600;
+const double windowHeight = 1000;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Navigation and routing');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
   }
 }
